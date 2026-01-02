ab4c3ae779f2400b4bb034f8276bff3f9d3a80b7
xinxin <1092203240@qq.com>
2026-01-02 15:19:48 +0800

    删除掉原来的前端

--- star-pivot-ui/src/views/error/403/index.vue
+++
@@ -1,8 +0,0 @@
-<!-- 403页面 -->
-<template>
-  404 页面
-</template>
-
-<script setup lang="ts">
-  defineOptions({ name: 'Exception403' })
-</script>
--- star-pivot-ui/src/views/error/404/index.vue
+++
@@ -1,7 +0,0 @@
-<!-- 404页面 -->
-<template>
-  404 页面
-</template>
-
-<script setup lang="ts">
-</script>
