9f598412d3a11b47fb3e28d9e13fc2ed3c5d4368
xinxin-star1998 <18834581124@163.com>
2026-03-04 16:04:32 +0800

    无效导入删除

--- star-pivot-controller/src/main/java/com/star/pivot/controller/SysRoleController.java
+++ star-pivot-controller/src/main/java/com/star/pivot/controller/SysRoleController.java
@@ -22,7 +22,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
