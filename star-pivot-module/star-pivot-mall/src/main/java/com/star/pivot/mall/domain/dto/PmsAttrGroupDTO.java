9d6c26f1ab802d2e66e2a86ff9e950554904cdf8
xinxin-star1998 <18834581124@163.com>
2026-05-18 23:07:02 +0800

    fix: 修复增删改对应的日志类型 refactor: 导入导出下载模版重构 feature: 商城系统部分完善（未完结） docs/*: 文档变更

--- star-pivot-module/star-pivot-mall/src/main/java/com/star/pivot/mall/domain/dto/PmsAttrGroupDTO.java
+++ star-pivot-module/star-pivot-mall/src/main/java/com/star/pivot/mall/domain/dto/PmsAttrGroupDTO.java
@@ -1,8 +1,5 @@
 package com.star.pivot.mall.domain.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
